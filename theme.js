fd55a4d0f2e525eaaa568a54bac1d731db9d6224
hontas <lundin.pontus@gmail.com>
2019-02-10 01:32:19 +0100

    add live code and code view

--- theme.js
+++ theme.js
@@ -1,12 +1,24 @@
-import theme from 'mdx-deck/themes'
+import { yellow as theme } from 'mdx-deck/themes';
 
 export default {
   ...theme,
-
+  liveCode: {
+    container: {
+      // container styles
+    },
+    editor: {
+      // editor styles
+    },
+    preview: {
+      // preview styles
+    },
+    error: {
+      // error styles
+    }
+  }
   // Customize your presentation theme here.
   //
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
-}
+};
